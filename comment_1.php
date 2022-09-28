75bb0506aeac0df2b24cdb97b8823b5887092256
Ncitbabeh <ncitbabeh@gmail.com>
2022-09-28 15:32:18 +0700

    slebew

---
+++ comment_1.php
@@ -0,0 +1,7 @@
+<?php
+echo "<p>Hello Dunia!</p>";
+// This is a single-line comment 
+
+echo "<p>saya belajar php!</p>";
+echo "<p>ini program pertama saya!</p>";
+?>
---
+++ comment_2.php
@@ -0,0 +1,5 @@
+<?php
+echo "Hello Dunia\n";
+echo "ini baris pertama\n";
+echo "ini baris kedua!\n";
+?>
